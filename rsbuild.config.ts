da150a9479341b509cad71b246607b2ac55618ac
M Sandoval <msandoval@Ms-MacBook-Pro.local>
2025-11-21 01:19:10 -0300

    build: create project and installation dependencies

---
+++ rsbuild.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+
+// Docs: https://rsbuild.rs/config/
+export default defineConfig({
+  plugins: [pluginReact()],
+
+  source: {
+    entry: {
+      index: './src/app/main.tsx',
+    },
+  },
+
+  resolve: {
+    alias: { '@': './src', '@/public': './public' },
+    extensions: ['.js', '.ts', '.tsx', '.css', '.html'],
+  },
+
+  html: { template: './static/index.html' },
+});
