2d3471603f802717959a74dc8752833651f600c4
M Sandoval <msandoval@Ms-MacBook-Pro.local>
2025-11-28 00:12:26 -0300

    chore: add barrel by service url

--- src/infra/index.ts
+++ src/infra/index.ts
@@ -1,10 +1,7 @@
-// repositorios
+export * from './repositories/familiarApi.repository';
 export * from './repositories/formacionGeneralApi.respository';
 export * from './repositories/generoApi.repository';
 export * from './repositories/preMatriculaApi.repository';
 export * from './repositories/tipoFamiliarApi.repository';
 
-// servicios
 export * from './services/calculateDV.service';
-
-// http servicios
