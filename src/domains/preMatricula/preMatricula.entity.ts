e4e0acdc09e42ac589febf9f175cf6d498b24aab
M Sandoval <msandoval@Ms-MacBook-Pro.local>
2025-11-30 17:47:26 -0300

    fix: add properties to display student data records

--- src/domains/preMatricula/preMatricula.entity.ts
+++ src/domains/preMatricula/preMatricula.entity.ts
@@ -1,6 +1,10 @@
 export interface PreMatriculaResponse {
   estado_ficha_matricula: number;
   fecha_prematricula: string;
+  grado_a_matricularse: number;
+  nombres: string;
+  apellido_paterno: string;
+  apellido_materno: string;
 }
 
 export interface PreMatriculaApiResponse {
